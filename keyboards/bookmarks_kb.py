ca5e7f17c60e8c03518a02434155603a040307c5
ilia <iliaman1993@gmail.com>
2023-02-22 15:49:13 +0300

    subdued the keyboard with bookmarks

--- keyboards/bookmarks_kb.py
+++ keyboards/bookmarks_kb.py
@@ -1,37 +1,43 @@
-from aiogram.types import InlineKeyboardButton
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from lexicon.ru import LEXICON
 from services.file_handling import book
 
 
-def create_bookmarks_keyboard(*args: int) -> InlineKeyboardBuilder:
+def create_bookmarks_keyboard(*args: int) -> InlineKeyboardMarkup:
     # Создаем объект клавиатуры
     bookmarks_kb: InlineKeyboardBuilder = InlineKeyboardBuilder()
     # Наполняем клавиатуру кнопками-закладками в порядке возрастания
     for button in sorted(args):
-        bookmarks_kb.add(InlineKeyboardButton(
+        bookmarks_kb.row(InlineKeyboardButton(
                             text=f'{button} - {book[button][:100]}',
                             callback_data=str(button)))
     # Добавляем в клавиатуру в конце две кнопки "Редактировать" и "Отменить"
-    bookmarks_kb.add(InlineKeyboardButton(
-                            text=LEXICON['edit_bookmarks_button'],
-                            callback_data='edit_bookmarks'),
-                     InlineKeyboardButton(text=LEXICON['cancel'],
-                                          callback_data='cancel'))
-    return bookmarks_kb
+    bookmarks_kb.row(InlineKeyboardButton(
+        text=LEXICON['edit_bookmarks_button'],
+        callback_data='edit_bookmarks'),
+        InlineKeyboardButton(
+            text=LEXICON['cancel'],
+            callback_data='cancel'
+        ),
+        width=2
+    )
+    return bookmarks_kb.as_markup()
 
 
-def create_edit_keyboard(*args: int) -> InlineKeyboardBuilder:
+def create_edit_keyboard(*args: int) -> InlineKeyboardMarkup:
     # Создаем объект клавиатуры
     bookmarks_kb: InlineKeyboardBuilder = InlineKeyboardBuilder()
     # Наполняем клавиатуру кнопками-закладками в порядке возрастания
     for button in sorted(args):
-        bookmarks_kb.add(InlineKeyboardButton(
+        bookmarks_kb.row(InlineKeyboardButton(
                     text=f'{LEXICON["del"]} {button} - {book[button][:100]}',
                     callback_data=f'{button}del'))
     # Добавляем в конец клавиатуры кнопку "Отменить"
-    bookmarks_kb.add(InlineKeyboardButton(
-                        text=LEXICON['cancel'],
-                        callback_data='cancel'))
-    return bookmarks_kb
+    bookmarks_kb.row(InlineKeyboardButton(
+        text=LEXICON['cancel'],
+        callback_data='cancel'),
+        width=2
+    )
+    return bookmarks_kb.as_markup()
--- keyboards/pagination_kb.py
+++ keyboards/pagination_kb.py
@@ -1,13 +1,14 @@
-from aiogram.types import InlineKeyboardButton
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from lexicon.ru import LEXICON
 
 
 # Функция, генерирующая клавиатуру для страницы книги
-def create_pagination_keyboard(*buttons: str) -> InlineKeyboardBuilder:
+def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
     # Создаем объект клавиатуры
     pagination_kb: InlineKeyboardBuilder = InlineKeyboardBuilder()
     # Наполняем клавиатуру кнопками
-    pagination_kb.row(*[InlineKeyboardButton(text=LEXICON[button] if button in LEXICON else button,
-                                             callback_data=button) for button in buttons])
-    return pagination_kb
+    pagination_kb.row(*[InlineKeyboardButton(
+        text=LEXICON[button] if button in LEXICON else button,
+        callback_data=button) for button in buttons])
+    return pagination_kb.as_markup()
